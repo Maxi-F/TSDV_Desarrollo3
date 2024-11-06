4b69935dfe0c217b24775c62cd1f224ccba3e88a
Nicodragon25 <nicolasariellaure@gmail.com>
2024-11-05 21:39:18 -0300

    Fix afterImage alpha blend and offset

--- Assets/Scripts/VFX/AfterImage/CustomTrail.cs
+++ Assets/Scripts/VFX/AfterImage/CustomTrail.cs
@@ -9,11 +9,12 @@ namespace VFX.AfterImage
     {
         [SerializeField] private float cloneDuration = 1f;
         [SerializeField] private float timeBetweenInstances = 0.1f;
+        [SerializeField] private Vector3 offset;
 
         private bool _shouldSpawnImages;
         private Coroutine _startCoroutine;
 
-        
+
         public void StartSpawn()
         {
             if (_startCoroutine != null)
@@ -37,7 +38,7 @@ namespace VFX.AfterImage
                 yield return new WaitForSeconds(timeBetweenInstances);
 
                 GameObject afterImage = AfterImagePool.Instance.GetPooledObject();
-                afterImage.transform.SetPositionAndRotation(transform.position, transform.rotation);
+                afterImage.transform.SetPositionAndRotation(transform.position + offset, transform.rotation);
                 afterImage.SetActive(true);
                 StartCoroutine(FadeOut(afterImage));
             }
@@ -45,17 +46,30 @@ namespace VFX.AfterImage
 
         IEnumerator FadeOut(GameObject afterImage)
         {
-            Material material = afterImage.GetComponentInChildren<SkinnedMeshRenderer>().material;
+            Material oldMat = afterImage.GetComponentInChildren<SkinnedMeshRenderer>().material;
+            Material diffuseMaterial = Instantiate(oldMat);
             float elapsedTime = 0f;
-            Color initialColor = material.color;
+            Color initialColor = diffuseMaterial.color;
+            SkinnedMeshRenderer[] meshRenderers = afterImage.GetComponentsInChildren<SkinnedMeshRenderer>();
             while (elapsedTime < cloneDuration)
             {
                 elapsedTime += Time.deltaTime;
-                float alpha = Mathf.Lerp(1f, 0f, elapsedTime / cloneDuration);
-                material.color = new Color(initialColor.r, initialColor.g, initialColor.b, alpha);
+                float alpha = Mathf.Lerp(initialColor.a, 0f, elapsedTime / cloneDuration);
+                diffuseMaterial.color = new Color(initialColor.r, initialColor.g, initialColor.b, alpha);
+                foreach (SkinnedMeshRenderer meshRenderer in meshRenderers)
+                {
+                    meshRenderer.material = diffuseMaterial;
+                }
+
                 yield return null;
             }
 
+            foreach (SkinnedMeshRenderer meshRenderer in meshRenderers)
+            {
+                meshRenderer.material = oldMat;
+            }
+
+            Destroy(diffuseMaterial);
             afterImage.SetActive(false);
         }
     }
