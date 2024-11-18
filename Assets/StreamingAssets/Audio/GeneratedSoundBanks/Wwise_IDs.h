258953e17c1935da06e28437a83cd12137247882
Zenith Infinitive GameAudio <zenithinfinitive@gmail.com>
2024-11-17 23:44:41 -0500

    SA_Lv1_EV_UI_Buttons_Main Implemented

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,34 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID SA_EV_LV0_MX_MENU_MAIN = 1724583484U;
-        static const AkUniqueID SA_EV_LV1_MX_GAMEPLAY = 3130449108U;
+        static const AkUniqueID SA_LV0_EV_MX_MENU_MAIN = 3137054506U;
+        static const AkUniqueID SA_LV1_EV_MX_GAMEPLAY = 3937289600U;
+        static const AkUniqueID SA_LV1_EV_UI_BUTTONS_MAIN = 3778692734U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace SA_LV1_ST_AX_CITY
+        {
+            static const AkUniqueID GROUP = 1552260201U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NIGHT = 1011622525U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID RAIN = 2043403999U;
+            } // namespace STATE
+        } // namespace SA_LV1_ST_AX_CITY
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SA_RTPC_BIKE_BOSSLV1_SPEED = 3673391112U;
+        static const AkUniqueID SA_RTPC_BIKE_INITIAL_DASH = 4217154045U;
+        static const AkUniqueID SA_RTPC_BIKE_INITIAL_SPEED = 3744839874U;
+        static const AkUniqueID SA_RTPC_BOSSLV1_TICK = 4244705786U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -25,7 +50,11 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AX = 1886858530U;
+        static const AkUniqueID DX = 1836525785U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MX = 1685527054U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
