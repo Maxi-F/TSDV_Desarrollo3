e74904c9de9fe9865b2748a9ef72f6f2ef88c73e
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-11-20 19:56:50 -0300

    Add music transitions to play & between phases

--- Assets/Scripts/Sounds/Wwise_IDs.cs
+++ Assets/Scripts/Sounds/Wwise_IDs.cs
@@ -1,10 +1,101 @@
 namespace AK
 {
+    
     public class EVENTS
     {
-        public static uint SA_EV_LV0_MX_MENU_MAIN = 1724583484U;
-        public static uint SA_EV_LV1_MX_GAMEPLAY = 3130449108U;
+        public static uint PAUSE_A_MX_TODO_95_BPM_V1A = 1692209436U;
+        public static uint PLAY_A_MX_TODO_95_BPM_V1A = 2817510498U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_BOMB_A_TICK = 965991614U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_BOMB_BOSS_HIT = 258404240U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_BOMB_OUT = 2354958303U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_BOMB_PLAYER_HIT = 2918316816U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_DEBRIS = 1768746087U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_DEBRIS_BREAK = 176071335U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_LASER_BURN = 1247021505U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_LASER_HOVER = 3592771044U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_LASER_OFF = 1745979437U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_LASER_ON = 1065709921U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_LAUNCHER_EXPLOTIONS = 2213470130U;
+        public static uint SA_LV1_EV_BOSSLV1_ATTACK_LAUNCHER_PLASMA = 3877748683U;
+        public static uint SA_LV1_EV_BOSSLV1_BIKE_ENGINE = 2983515949U;
+        public static uint SA_LV1_EV_BOSSLV1_BIKE_ENGINE_ESCAPE = 659427289U;
+        public static uint SA_LV1_EV_BOSSLV1_BIKE_ENGINE_WEAK = 791370784U;
+        public static uint SA_LV1_EV_BOSSLV1_DAMAGE_DIE_EXPLOTION = 3075663134U;
+        public static uint SA_LV1_EV_BOSSLV1_DAMAGE_SWORDHIT = 1086047977U;
+        public static uint SA_LV1_EV_BOSSLV1_MOVEMENT_SWING = 2539702551U;
+        public static uint SA_LV1_EV_BOSSLV1_SHIELD_OFF = 923788466U;
+        public static uint SA_LV1_EV_BOSSLV1_SHIELD_ON = 3835157588U;
+        public static uint SA_LV1_EV_DX_BOSSLV1_DIE = 2624148046U;
+        public static uint SA_LV1_EV_DX_BOSSLV1_HURT = 2655358455U;
+        public static uint SA_LV1_EV_DX_CX_INTRO = 401201879U;
+        public static uint SA_LV1_EV_DX_PLAYER1_ATTACK = 3455697998U;
+        public static uint SA_LV1_EV_DX_PLAYER1_DIE = 2192991792U;
+        public static uint SA_LV1_EV_DX_PLAYER1_HURT = 926986689U;
+        public static uint SA_LV1_EV_DX_UI_WARNING = 2821358494U;
+        public static uint SA_LV1_EV_MINION_ATTACK_DASH = 805092198U;
+        public static uint SA_LV1_EV_MINION_BLADE = 52284575U;
+        public static uint SA_LV1_EV_MINION_DAMAGE_EXPLODE = 4259830062U;
+        public static uint SA_LV1_EV_MINION_DAMAGE_IMPACT = 3093941657U;
+        public static uint SA_LV1_EV_MINION_DAMAGE_WEAK = 3281992927U;
+        public static uint SA_LV1_EV_MINION_ENGINE = 2404778933U;
+        public static uint SA_LV1_EV_MINION_LASERPOINT = 2022848226U;
+        public static uint SA_LV1_EV_OBJ_DESTROY_FENCE = 1439609548U;
+        public static uint SA_LV1_EV_OBJ_DESTROY_GENERIC = 694517172U;
+        public static uint SA_LV1_EV_PLAYER1_BIKE_INITIAL_DASH = 29769706U;
+        public static uint SA_LV1_EV_PLAYER1_BIKE_INITIAL_ENGINE = 1573837084U;
+        public static uint SA_LV1_EV_PLAYER1_DIE_EXPLOTION = 876086836U;
+        public static uint SA_LV1_EV_PLAYER1_HEALING = 4117304617U;
+        public static uint SA_LV1_EV_SWORD_WHOOSH = 3314116518U;
+        public static uint SA_LV1_EV_UI_BUTTONS_MAIN = 3778692734U;
+        public static uint SA_LV1_EV_UI_TEXT_TYPING = 1221627396U;
+        public static uint SA_LV1_PLAYER1_DAMAGE_HIT = 1161613028U;
+        public static uint UNPAUSE_A_MX_TODO_95_BPM_V1A = 659047857U;
     } // public class EVENTS
+    
+    public class STATES
+    {
+        public class ESTADOS_MUSIC
+        {
+            public static uint GROUP = 2182109522U;
+
+            public class STATE
+            {
+                public static uint CINEMATICA = 877642353U;
+                public static uint FASE_1 = 3821454346U;
+                public static uint FASE_1_BOSS = 2657527206U;
+                public static uint FASE_2 = 3821454345U;
+                public static uint FASE_2_BOSS = 406273255U;
+                public static uint FASE_3 = 3821454344U;
+                public static uint FASE_3_BOSS = 2536681412U;
+                public static uint GAME_OVER = 1432716332U;
+                public static uint MENU_CREDITS = 531391871U;
+                public static uint NONE = 748895195U;
+            } // public class STATE
+        } // public class ESTADOS_MUSIC
+
+        public class SA_LV1_ST_AX_CITY
+        {
+            public static uint GROUP = 1552260201U;
+
+            public class STATE
+            {
+                public static uint NIGHT = 1011622525U;
+                public static uint NONE = 748895195U;
+                public static uint RAIN = 2043403999U;
+            } // public class STATE
+        } // public class SA_LV1_ST_AX_CITY
+
+    } // public class STATES
+
+    public class GAME_PARAMETERS
+    {
+        public static uint BIKE_SPEED = 2047381990U;
+        public static uint MASTER = 4056684167U;
+        public static uint SA_RTPC_BIKE_BOSSLV1_SPEED = 3673391112U;
+        public static uint SA_RTPC_BIKE_INITIAL_DASH = 4217154045U;
+        public static uint SA_RTPC_BIKE_INITIAL_SPEED = 3744839874U;
+        public static uint SA_RTPC_BOSSLV1_TICK = 4244705786U;
+    } // public class GAME_PARAMETERS
 
     public class BANKS
     {
@@ -14,7 +105,11 @@ namespace AK
 
     public class BUSSES
     {
+        public static uint AX = 1886858530U;
+        public static uint DX = 1836525785U;
         public static uint MASTER_AUDIO_BUS = 3803692087U;
+        public static uint MX = 1685527054U;
+        public static uint SFX = 393239870U;
     } // public class BUSSES
 
     public class AUDIO_DEVICES
